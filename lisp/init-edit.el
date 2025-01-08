42810332cbbbc8d6ba4c7a49522c4c0596b5f26d
Aaron Wu <demonaarwu@tuta.io>
2025-01-08 19:00:44 +0800

    Set Untabify Method Hungry

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -1,6 +1,7 @@
 ;; init-edit.el -- Configuration for edit
 
 (electric-pair-mode 1)
+(setq backward-delete-char-untabify-method 'hungry)
 
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
