bf6a0e45ee21f5856b8639605d3e5434a18112d5
Aaron Wu <demonaarwu@tuta.io>
2025-01-11 10:32:35 +0800

    feat: remove useless packages and improve performances

--- init.el
+++ init.el
@@ -1,7 +1,5 @@
 ;; init.el -- Root of all configs
 
-(setq gc-cons-threshold (* 128 1024 1024))
-
 (defconst aaw-emacs-d (file-name-as-directory user-emacs-directory)
   "Directory of emacs.d.")
 
@@ -10,29 +8,42 @@
 
 (add-to-list 'load-path aaw-lisp-dir)
 
-(let ((file-name-handler-alist nil))
+;;; Code:
+(setq
+ ;; 不要缩放frame.
+ frame-inhibit-implied-resize t
+ ;; 默认用最简单的模式
+ initial-major-mode 'fundamental-mode
+ ;; 不要自动启用package
+ package-enable-at-startup nil
+ package--init-file-ensured t)
+
+
+(let ((file-name-handler-alist nil)
+      (gc-cons-threshold (* 128 1024 1024)))
   (require 'init-misc)
   (require 'init-edit)
   (require 'init-theme)
   (require 'init-packages)
-  (require 'init-vertico)
-  (require 'init-term)
-  (require 'init-evil)
-  (require 'init-autosave)
-  ;; (require 'init-lsp)
-  (require 'init-eglot)
-  (require 'init-company)
   (require 'init-modeline)
-  (require 'init-clipboard)
+  (require 'init-vertico)
+  (require 'init-company)
+  (require 'init-evil)
   (require 'init-windows)
-  (require 'init-org)
-  (require 'init-epub)
-  (require 'init-magit)
-  (require 'init-python)
-  (require 'init-c)
-  (require 'init-cl)
-  (require 'init-rust)
-  (require 'init-shell)
+  (require 'init-eglot)
+  (run-with-idle-timer
+   1 nil
+   #'(lambda ()
+       (require 'init-clipboard)
+       (require 'init-org)
+       (require 'init-term)
+       (require 'init-autosave)
+       (require 'init-magit)
+       (require 'init-python)
+       ;; (require 'init-c)
+       (require 'init-cl)
+       (require 'init-rust)))
+
   (setq custom-file (expand-file-name "custom-set-variables.el" user-emacs-directory))
   (if (file-exists-p "~/.emacs.d/custom.el")
       (load "~/.emacs.d/custom.el"))
--- lisp/init-shell.el
+++
@@ -1,9 +0,0 @@
-;; init-shell.el -- Configuration for shell
-
-(use-package aweshell
-  :vc (:fetcher github :repo "manateelazycat/aweshell")
-  :init
-  (require 'aweshell))
-
-(provide 'init-shell)
-;; init-shell.el ends here
