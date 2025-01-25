7acf3a0354e6d6209c5fdcccba0817c6a9798135
Aaron Wu <demonaarwu@tuta.io>
2025-01-25 08:56:35 +0800

    feat: Add support to Venv

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -2,5 +2,14 @@
 
 (setq lsp-bridge-python-lsp-server 'pyright)
 
+(use-package pyvenv
+  :ensure t
+  :init
+  (setenv "WORKON_HOME" "~/.pyenv/versions")
+  (add-hook 'python-mode-hook (lambda ()
+                                (pyvenv-mode t)
+                                (pyvenv-activate "./venv")
+                                (eglot-ensure))))
+
 (provide 'init-python)
 ;; init-python.el ends here
