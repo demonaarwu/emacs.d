f2b33a67e00b4a245c58d5c8749fe961a8c050f7
Aaron Wu <demonaarwu@tuta.io>
2025-01-25 08:56:58 +0800

    feat: change navigation commands of Vertico

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -23,8 +23,8 @@
   ;; More convenient directory navigation commands
   :bind (:map vertico-map
               ("RET" . vertico-directory-enter)
-              ;; ("DEL" . vertico-directory-delete-char)
-              ("DEL" . vertico-directory-delete-word))
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
   ;; Tidy shadowed file names
   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
 
