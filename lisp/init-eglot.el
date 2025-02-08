f2943c0293b9396bc562f725e1f508afd1b1d5f3
Aaron Wu <demonaarwu@tuta.io>
2025-02-08 10:43:04 +0800

    feat: change Eglot Configuration

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -1,20 +1,18 @@
 ;; init-eglot -- Configuration for Eglot
 
 (use-package eglot
-  :hook (prog-mode . (lambda ()
-                       (unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode 'snippet-mode)
-                         (eglot-ensure))))
   :init
   (setq read-process-output-max (* 1024 1024))
   (setq eglot-autoshutdown t
         eglot-events-buffer-size 0
         eglot-send-changes-idle-time 0.5))
 
-(when (executable-find "emacs-lsp-booster")
-  (use-package eglot-booster
-    :vc (:url "https://github.com/jdtsmith/eglot-booster")
-    :after eglot
-    :init (eglot-booster-mode)))
+(use-package eglot-booster
+  :vc (:url "https://github.com/jdtsmith/eglot-booster")
+  :init
+  (with-eval-after-load 'eglot
+    (eglot-booster-mode)
+    (flymake-mode -1)))
 
 (provide 'init-eglot)
 ;; init-eglot.el ends here
