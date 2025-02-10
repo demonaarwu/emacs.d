44b66d087cb57de50cc5b366a8088fee09bca78a
Aaron Wu <demonaarwu@tuta.io>
2025-02-10 12:04:10 +0800

    feat: add functions for delete indent

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -12,5 +12,23 @@
   :diminish
   :hook (after-init . global-auto-revert-mode))
 
+(defun intellij-backspace (arg)
+  (interactive "*P")
+  (if (or (region-active-p) (not (looking-back "^[[:space:]]*" (line-beginning-position))))
+      (backward-delete-char-untabify (prefix-numeric-value arg))
+    (let* ((beg (point))
+           (end (progn (indent-for-tab-command) (point))))
+      (when (<= beg end)
+        (if (save-excursion (forward-line -1) (line-blank-p))
+            (progn (delete-region (line-beginning-position 0) (line-beginning-position)) (back-to-indentation))
+          (delete-indentation))))))
+
+(defun line-blank-p ()
+  (save-excursion
+    (beginning-of-line)
+    (looking-at "[[:space:]]*$")))
+
+(define-key prog-mode-map (kbd "<backspace>") #'intellij-backspace)
+
 (provide 'init-edit)
 ;; init-edit.el ends here
