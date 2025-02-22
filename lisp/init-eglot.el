882b798b7f11d5272db1142dd11592dc3aca8140
Aaron Wu <demonaarwu@tuta.io>
2025-02-22 14:56:51 +0800

    feat: disable eglot features

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -1,11 +1,17 @@
 ;; init-eglot -- Configuration for Eglot
 
 (use-package eglot
+  :general
+  (aaw-leader-def
+    :states 'normal
+    "rn" 'eglot-rename)
   :init
   (setq read-process-output-max (* 1024 1024))
   (setq eglot-autoshutdown t
         eglot-events-buffer-size 0
-        eglot-send-changes-idle-time 0.5))
+        eglot-send-changes-idle-time 0.5)
+  (setq eglot-ignored-server-capabilities '(:documentHighlightProvider
+                                            :codeLensProvider)))
 
 (use-package eglot-booster
   :vc (:url "https://github.com/jdtsmith/eglot-booster")
