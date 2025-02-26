b789e8083cb435a19f37ae478ee05a67df0d3667
Aaron Wu <demonaarwu@tuta.io>
2025-02-26 11:34:58 +0800

    feat: make Emacs faster when opening large files

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -30,5 +30,11 @@
 
 (global-set-key (kbd "<backspace>") #'intellij-backspace)
 
+(setq-default bidi-display-reordering nil)
+(setq bidi-inhibit-bpa t
+      long-line-threshold 1000
+      large-hscroll-threshold 1000
+      syntax-wholeline-max 1000)
+
 (provide 'init-edit)
 ;; init-edit.el ends here
