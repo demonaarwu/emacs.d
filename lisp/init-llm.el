967884829f891e3f93b6beedd299a1d83ed18711
Aaron Wu <demonaarwu@tuta.io>
2025-06-06 10:48:36 +0800

    feat: refracting gptel's setup and adding GitHub Copilot

--- lisp/init-llm.el
+++ lisp/init-llm.el
@@ -1,18 +1,26 @@
 ;;; init-llm.el --- Configuration for LLMs  -*- lexical-binding: t -*-
 
+(defun aaw-gemini-setup ()
+  (gptel-make-gemini "Gemini" :key GEMINI-API-KEY :stream t)
+  (setq gptel-model 'gemini-2.0-flash)
+  (setq gptel-backend (gptel-make-gemini "Gemini"
+                        :key GEMINI-API-KEY
+                        :stream t)))
+
+(defun aaw-copilot-setup ()
+  (gptel-make-gh-copilot "Copilot")
+  (setq gptel-model 'claude-3.7-sonnet
+        gptel-backend (gptel-make-gh-copilot "Copilot")))
+
+(defun aaw-llm-model-setup (model)
+  (if (equal model "Gemini")
+      (aaw-gemini-setup)
+    (aaw-copilot-setup)))
+
 (use-package gptel
   :ensure t
   :init
-  ;; OPTIONAL configuration
-  ;; :key can be a function that returns the API key.
-  (gptel-make-gemini "Gemini" :key GEMINI-API-KEY :stream t)
-
-  ;; OPTIONAL configuration
-  (setq
-   gptel-model 'gemini-2.0-flash
-   gptel-backend (gptel-make-gemini "Gemini"
-                   :key GEMINI-API-KEY
-                   :stream t))
+  (aaw-llm-model-setup "Gemini")
   (setq gptel-default-mode 'org-mode)
   (setq gptel-prompt-prefix-alist '((markdown-mode . "### Prompt:") (org-mode . "*** Prompt: \n") (text-mode . "###  Prompt: \n")))
   (setq gptel-response-prefix-alist '((markdown-mode . #1="") (org-mode . "*** Response: \n") (text-mode . #1#)))
