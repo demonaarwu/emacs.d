47e4923a0071872030a46319b77c944777c34f9a
Aaron Wu <demonaarwu@tuta.io>
2025-06-08 11:55:24 +0800

    feat: adjust gptel's directives

--- lisp/init-llm.el
+++ lisp/init-llm.el
@@ -17,9 +17,25 @@
       (aaw-gemini-setup)
     (aaw-copilot-setup)))
 
+(defun aaw-llm-directives-setup ()
+  (let ((default-directive
+         "You are a large language model living in Emacs and a helpful assistant. Respond thoroughly")
+        (programming-directive
+         "You are a large language model and a careful programmer. Provide code and only code as output without any additional text, prompt or note.")
+        (writing-directive
+         "You are a large language model and a writing assistant. Respond concisely.")
+        (socrates-directive
+         "You are a tutor that always responds in the Socratic style. You never give the student the answer, but always try to ask just the right question to help them learn to think for themselves. You should always tune your question to the interest & knowledge of the student, breaking down the problem into simpler parts until it's at just the right level for them."))
+    (setq gptel-directives
+          `((default . ,default-directive)
+            (programming . ,programming-directive)
+            (writing . ,writing-directive)
+            (socrates . ,socrates-directive)))))
+
 (use-package gptel
   :ensure t
   :init
+  (aaw-llm-directives-setup)
   (aaw-llm-model-setup "Gemini")
   (setq gptel-default-mode 'org-mode)
   (setq gptel-prompt-prefix-alist '((markdown-mode . "### Prompt:") (org-mode . "*** Prompt: \n") (text-mode . "###  Prompt: \n")))
