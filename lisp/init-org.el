3e5921e27c1a6d3cbfd393bdeb071a875639f311
Aaron Wu <demonaarwu@tuta.io>
2025-06-11 07:53:35 +0800

    refract: lisp/init-org.el

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,42 +1,18 @@
 ;; init-org.el -- Configuration for Org Mode
 
-(setq org-agenda-files '("~/org/"))
-
-(defun insert-title ()
-  (interactive)
-  (insert "#+title: \n")
-  (insert "#+date: " (format-time-string "%Y-%m-%d")))
-
-(setq org-default-notes-file "~/org/inbox.org")
-
-;; When a TODO is set to a done state, record a timestamp
-(setq org-log-done 'time)
-
-;; Follow the links
-(setq org-return-follows-link t)
-
-(setq truncate-lines nil)
-
-;; Org Capture Configuration
-(setq org-capture-templates nil)
-
-(defun get-words-learned-today ()
-  (read-string "English Words: "))
-
-(defun get-math-problems-done-today ()
-  (read-string "Math Problems: "))
-
-(setq aaw-habit-tracker
-      '("h"
-        "Habit Tracker"
-        table-line
-        (file+headline "habit-tracker.org" "Habit Tracker")
-        "| %(format-time-string \"%Y-%m-%d\") | %(get-words-learned-today)| %(get-math-problems-done-today)|"))
-
-(add-to-list 'org-capture-templates aaw-habit-tracker)
-
 (use-package org
   :ensure nil
+  :init
+  (setq org-startup-with-latex-preview t)
+  (setq org-agenda-files '("~/org/"))
+  (setq org-default-notes-file "~/org/inbox.org")
+  ;; When a TODO is set to a done state, record a timestamp
+  (setq org-log-done 'time)
+  ;; Follow the links
+  (setq org-return-follows-link t)
+  (setq truncate-lines nil)
+  ;; Org Capture Configuration
+  (setq org-capture-templates nil)
   :general
   (aaw-leader-def
     :states 'normal
