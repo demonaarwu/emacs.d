ff98b9defa4ed41a419230a61ca86e73e7995fe6
Aaron Wu <demonaarwu@tuta.io>
2025-06-19 19:35:36 +0800

    feat: add wider keybindings support for evil-mode

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -3,6 +3,7 @@
 (use-package evil
   :ensure t
   :init
+  (setq evil-want-keybinding nil)
   (add-hook 'after-init-hook 'evil-mode)
   (with-eval-after-load 'evil
     (evil-set-initial-state 'vterm-mode 'insert)))
@@ -13,7 +14,8 @@
   :init
   (evil-escape-mode 1)
   (setq-default evil-escape-key-sequence "kj")
-  (setq-default evil-escape-delay 0.2))
+  (setq-default evil-escape-delay 0.2)
+  (add-to-list 'evil-escape-excluded-major-modes 'magit-status-mode))
 
 (use-package evil-nerd-commenter
   :ensure t
@@ -31,6 +33,12 @@
   :ensure t
   :hook
   (after-init . which-key-mode))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :init
+  (evil-collection-init))
 
 (provide 'init-evil)
 ;; init-evil.el ends here
